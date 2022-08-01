e4f0edad46ac761e386157980f839594da63284c
lavilladaa <lauravilladaa16@gmail.com>
2022-08-01 15:50:35 -0500

    Create the new and create methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article # if the article is saved successfully
+    else
+      render :new, status: :unprocessable_entity # redisplays the form
+    end
+  end
 end
