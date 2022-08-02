88c0034b582b265b45289181cda604d4895276cf
Laura Villada <104602649+lavilladaa@users.noreply.github.com>
2022-08-02 07:49:38 -0500

    Delete unused code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  #get "/articles", to: "articles#index"
-  #get "/articles/:id", to: "articles#show" # to show the title and body
-
   # to replace the previous two lines with the "resources" method
   resources :articles 
 end
