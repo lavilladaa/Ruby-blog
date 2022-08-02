97be64ddccab2afd03d5d8c9c278fa0b0a299019
lavilladaa <lauravilladaa16@gmail.com>
2022-08-02 10:54:10 -0500

    Fix the typos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments, dependet: :destroy # to destroy the comments when the article is deleted
+  has_many :comments, dependent: :destroy # to destroy the comments when the article is deleted
 
   validates :title, presence: true # title must contain at least one non-whitespace character
   validates :body, presence: true, length: { minimum: 10 }
