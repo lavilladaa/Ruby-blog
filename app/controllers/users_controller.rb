d2d07aed90691acded8186f3bda56e36360fbfd5
lavilladaa <lauravilladaa16@gmail.com>
2022-08-05 10:37:45 -0500

    Create user profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    #@article = @user.articles.create(user_params)
+  end
+
+  # def index
+  #   @user = User.find(params[:article_id])
+
+  #   @article = @user.articles.create(user_params)
+  # end
+
+  private
+
+  # def user_params
+  #   # The create action can access with params[:article][:title]
+  #   params.require(:user).permit(:email, :password, :password_confirmation)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "articles#index"
+
+  resources :users
 
   resources :articles do
     resources :comments
