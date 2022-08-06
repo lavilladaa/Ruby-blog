d7914ff4885b73ac7996ee271a39103f474f8b14
lavilladaa <lauravilladaa16@gmail.com>
2022-08-06 12:47:42 -0500

    Create relationship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_05_193918) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_06_173722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    #t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-    #.string "user_id"
-    t.string "integer"
-    t.index ["user_id"], name: "index_articles_on_user_id"
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -36,6 +33,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_05_193918) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_05_193918) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
+    t.integer "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
