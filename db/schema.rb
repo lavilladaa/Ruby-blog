201515391837a6f8b41c1b3f17c8d6f079f7d352
lavilladaa <lauravilladaa16@gmail.com>
2022-08-06 12:35:46 -0500

    Fix the db problem for article model

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_05_193918) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.bigint "user_id", null: false
+    #t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
